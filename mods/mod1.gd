240046512097a8f4f1fab7d6f7cd95fd76622832
marcusmmmz <30752235+marcusmmmz@users.noreply.github.com>
2020-09-30 12:15:17 -0300

    Mod List

---
+++ mods/mod1.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# Não fiz ainda :pexe
+var mod_info = {
+	"name":"Nome do mod",
+}
+
+func _ready():
+	print("Mude esse texto")
+
+
---
+++ src/ModManager.gd
@@ -0,0 +1,23 @@
+extends Node
+#Esse aq é um singleton
+
+func _ready():
+	load_mods()
+
+func load_mods():
+	var dir = Directory.new()
+	var path = "./mods"
+	
+	var mods = []
+	
+	if dir.open(path) == OK:
+		dir.list_dir_begin(true)
+		var mod = dir.get_next()
+		while mod != "":
+			if mod.ends_with(".gd"):
+				mods.append( load(path + "/" + mod) )
+			
+			mod = dir.get_next()
+	
+	for mod in mods:
+		add_child( mod.new() )
