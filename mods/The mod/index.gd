cf1cf3bc5abd3431a267cf10b52137d9cfef0f6d
marcusmmmz <30752235+marcusmmmz@users.noreply.github.com>
2020-10-01 18:49:26 -0300

    Mods in folders, mod description and author

---
+++ mods/The mod/index.gd
@@ -0,0 +1,6 @@
+extends Node
+
+onready var root = get_node("/root/Main")
+
+func _ready():
+	print("Yes, it's working")
--- src/ModManager.gd
+++ src/ModManager.gd
@@ -1,22 +1,38 @@
 extends Node
 
+var mods = []
+
 func _ready():
 	load_mods()
 
-func load_mods():
+func list_dir(path):
 	var dir = Directory.new()
-	var path = "./mods"
 	
-	var mods = []
+	var files = []
 	
 	if dir.open(path) == OK:
 		dir.list_dir_begin(true)
-		var mod = dir.get_next()
-		while mod != "":
-			if mod.ends_with(".gd"):
-				mods.append( load(path + "/" + mod) )
-			
-			mod = dir.get_next()
+		var file = dir.get_next()
+		
+		while file != "":
+			files.append( file )
+			file = dir.get_next()
 	
-	for mod in mods:
-		add_child( mod.new() )
+	return files
+
+func load_mods():
+	var path = "./mods/"
+	for mod_dir in list_dir("./mods"):
+		
+		path += mod_dir + "/"
+		for dir_file in list_dir(path):
+			
+			match dir_file:
+				"index.gd":
+					add_child( load(path + "/index.gd").new() )
+				
+				"info.json":
+					var file = File.new()
+					file.open(path + "info.json", File.READ)
+					mods.append(parse_json(file.get_as_text()))
+					file.close()
